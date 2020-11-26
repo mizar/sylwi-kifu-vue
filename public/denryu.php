fbcbf292842d4f8106a7bd9fc7b4172c5a8495f6
Mizar <mizar.jp@gmail.com>
2020-11-26 21:40:43 +0900

    php relative

--- public/denryu.php
+++ public/denryu.php
@@ -3,11 +3,15 @@
 <head>
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width"/>
+<?php
+$uri = $_SERVER["SCRIPT_NAME"];
+$uri = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . substr($uri, 0, strrpos($uri, '/'));
+?>
 <meta property="og:title" content="第1回電竜戦 <?php echo htmlspecialchars($_GET["gn"]) ?> <?php echo $tesuuStr ?> <?php echo $mStr ?> まで"/>
 <meta property="og:description" content="第1回電竜戦 <?php echo htmlspecialchars($_GET["gn"]) ?> <?php echo $tesuuStr ?> <?php echo $mStr ?> まで"/>
 <meta property="og:site_name" content="電竜戦"/>
-<meta property="og:image" content="https://sylwi.mzr.jp/cimg_denryu.php?p=<?php echo htmlspecialchars($_GET["p"]) ?>"/>
-<meta property="og:url" content="https://sylwi.mzr.jp/#/<?php echo urlencode($_GET["tn"]) ?>/<?php echo urlencode($_GET["gi"]) ?>/<?php echo $tesuu ?>"/>
+<meta property="og:image" content="<?php echo $uri ?>/cimg_denryu.php?p=<?php echo htmlspecialchars($_GET["p"]) ?>"/>
+<meta property="og:url" content="<?php echo $uri ?>/#/<?php echo urlencode($_GET["tn"]) ?>/<?php echo urlencode($_GET["gi"]) ?>/<?php echo $tesuu ?>"/>
 <meta property="og:locale" content="ja_JP"/>
 <meta property="og:type" content="article"/>
 <meta name="twitter:card" content="summary_large_image"/>
@@ -22,7 +26,5 @@ body, iframe {
 }
 </style>
 </head>
-<body>
-  <iframe src="https://sylwi.mzr.jp/#/<?php echo urlencode($_GET["tn"]) ?>/<?php echo urlencode($_GET["gi"]) ?>/<?php echo $tesuu ?>"></iframe>
-</body>
+<body><iframe src="<?php echo $uri ?>/#/<?php echo urlencode($_GET["tn"]) ?>/<?php echo urlencode($_GET["gi"]) ?>/<?php echo $tesuu ?>"></iframe></body>
 </html>
