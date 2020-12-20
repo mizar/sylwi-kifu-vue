b3ee23ce6e50efe029cb73520b3ec36771af7795
Mizar <mizar.jp@gmail.com>
2020-12-20 21:58:29 +0900

    licenses-filter: lint guard

--- src/licenses-filter.ts
+++ src/licenses-filter.ts
@@ -1,18 +1,21 @@
 const lines: string[] = [];
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const reader = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-reader.on("line", (line: string) => { lines.push(line) });
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
 reader.on("close", () => {
-
   const data = JSON.parse(lines.join(""));
 
-  for (const [key, value] of Object.entries<any>(data)) {
+  for (const value of Object.values<{
+    [key: string]: string | undefined;
+  }>(data)) {
     value.path = undefined;
   }
 
   console.log(JSON.stringify(data, undefined, 2));
-
 });
